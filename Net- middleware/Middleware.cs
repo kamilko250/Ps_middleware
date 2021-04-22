c088b744c8a2877a847b174dcbf48b5646211bcb
kamilko250 <freshmultikk21@gmail.com>
2021-04-22 14:34:42 +0200

    zadanie

---
+++ Net- middleware/Middleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UAParser;
+
+namespace Net__middleware
+{
+    public class Middleware
+    {
+        private RequestDelegate _request;
+        public Middleware(RequestDelegate request)
+        {
+            _request = request;
+        }
+        public Task Invoke(HttpContext context)
+        {
+            var userAgent = context.Request.Headers["User-Agent"];
+            var uaParser = Parser.GetDefault();
+            ClientInfo client = uaParser.Parse(userAgent);
+            string browser = client.UserAgent.Family;
+            if (browser == "Edge" || browser == "IE" || browser == "EgdeChromium")
+            {
+                context.Response.WriteAsync($"<p>{browser } - Przegladarka nieobslugiwana</p>");
+            }
+            return _request(context);
+        }
+    }
+}
